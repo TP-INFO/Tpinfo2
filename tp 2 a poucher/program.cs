877763f91f0fb653d30c304abda4fd781d6ef9a0
MarouenKanoun <marouen1931@hotmail.com>
2017-03-23 13:01:55 +0100

    premier commit tp2

---
+++ tp 2 a poucher/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salarie sale = new Salarie("salah ", 300, 10014, 34000, 10);
+            sale.Calculesalire();
+            sale.nom = "xxxx";
+            sale.matricule = "222222";
+            sale.Calculesalire();
+
+        }
+    }
+}
---
+++ tp 2 a poucher/salairié.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication174
+{
+    class Salarie
+    {/// <summary>
+     ///  declarer les attribut 
+     /// </summary>
+        protected int matricule;
+        protected int categorie;
+        protected int service;
+        protected string nom;
+        protected double salaire;
+
+        public int Matricule
+        {
+            get { return matricule; }
+            set { matricule = value; }
+
+        }
+        public int Categorie
+        {
+            get { return categorie; }
+            set { categorie = value; }
+        }
+        public int Service
+        {
+            get { return service; }
+            set { service = value; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public double Salaire
+        {
+            get { return salaire; }
+            set { salaire = value; }
+        }
+
+
+
+
+
+        public Salarie(string lenom, double lesalire, int lamatricule, int leservice, int lacategorie)
+        {
+            Categorie = lacategorie;
+            Matricule = matricule;
+            Service = leservice;
+            Nom = lenom;
+            Salaire = lesalire;
+        }
+        public string  Calculesalire()
+        {
+            return $" le salaire de {nom} est de {salaire}";
+
+        }
+       
+        
+
+    }
+}
