abfbab5b82f6ce3243dd6fab3732d61eb03efc8a
HerveRimbaud <rvrimbaud34@gmail.com>
2017-03-29 16:25:54 +0200

    meth décodé v1

--- Tpinfo2/Program.cs
+++ Tpinfo2/Program.cs
@@ -10,7 +10,64 @@ namespace Tpinfo2
     {
         static void Main(string[] args)
         {
-            //First push
+            Console.WriteLine("Saisissez la phrase codée : ");
+            string phraseCodee = Console.ReadLine();
+            char[] ph = new char[phraseCodee.Length];
+            char let;
+            int decalage;
+            for (int i = 1; i <= 26; i++)
+            {
+                for (int j = 0; j < phraseCodee.Length; j++)
+                {
+                    let = phraseCodee[j];
+                    ph[j] = (char)(let + i);
+                    if (ph[j] > 'z')
+                    {
+                        ph[j] = (char)(ph[j] - 26);
+                    }                    
+                }
+                decalage = i;
+                Console.WriteLine(ph);
+                Console.WriteLine(decalage);
+                               
+            }
+            Console.WriteLine();
         }
+
+
+
+
+
+
+
+
+
+            //if (args.Length >= 1)
+            //{
+            //    switch (args[0])
+            //    {
+            //        case "Message encode":
+            //            AfficherMesEncode();
+            //            break;
+            //        case "Message decode":
+            //            AfficherMesDecode();
+            //            break;
+            //        default:
+            //            break;
+
+            //    }
+            //}
+
+        
+        //public static void AfficherMesEncode()
+        //{
+
+        //}
+
+        //public static void AfficherMesDecode()
+        //{
+
+        //}
+
     }
 }
