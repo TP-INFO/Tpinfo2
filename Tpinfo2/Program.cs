d760bff6fd90a54a43c1c37a8afaef22efcccae7
LucNtonga <luc@LUCNTONGA87BB>
2017-03-30 00:38:31 +0200

    v1.1 modif méthode dechiffrer par Bruteforce OK

--- Tpinfo2/Program.cs
+++ Tpinfo2/Program.cs
@@ -10,41 +10,60 @@ namespace Tpinfo2
 	{
 		static void Main(string[] args)
 		{
-			string phraseCode = "Ssfksdwsxjgflgfsvsmfslwehdwssflaimw";
-			//string phraseCode = "zzzzzzzzzzzzzzzzzzzzzzzzzzz";
-			int i = 0;
-			char[] tabDecod = new char[phraseCode.Length];
-			 
-			int cle = 8;
+			// initialisation de la valeur clé de chiffrement
+			int cle = 0;
+
+			// initialistion du nbre de clés de chiffrement
+			int count = 0;
 
 
-			foreach (char c in phraseCode)
+			// boucle do/while pour basé sur le nbre de clés
+			do
 			{
-				int nChiffre = phraseCode[i];
-				Console.WriteLine(nChiffre);
-				int enChiffre = nChiffre + cle;
+				string phraseCode = "Wtfltdxtykgfmgftwtnftmxehdxttfmbinx";
+				//string phraseCode = "Ssfksdwsxjgflgfsvsmfslwehdwssflaimw";
+				//string phraseCode = "kvmvyzvgzioznvhzovhjmkcjnzn";
 
-				if (enChiffre > 122)
+				//Console.WriteLine(phraseCode);
+
+				int i = 0;
+				char[] tabDecod = new char[phraseCode.Length];
+			
+				foreach (char c in phraseCode)
 				{
-					enChiffre = (enChiffre + (cle - (26+cle)));
+					int nChiffre = phraseCode[i];  //  i = nbre de char de la phrase
+					//Console.WriteLine(nChiffre);
+					int enChiffre = nChiffre + cle;
+
+					if (enChiffre > 122)
+					{
+						enChiffre = (enChiffre - 26);
+					}
+
+					i++;
+					//Console.WriteLine($"lettre originale :{phraseCode[i - 1]}");
+					//Console.WriteLine($" :{enChiffre}");
+					char newLettre = Convert.ToChar(enChiffre);
+					tabDecod[i-1] = newLettre;
+					//Console.WriteLine($"Nouvelle lettre :{newLettre}");
 				}
 
-				i++;
-				Console.WriteLine($"lettre originale :{phraseCode[i - 1]}");
-				Console.WriteLine($" :{enChiffre}");
-				char newLettre = Convert.ToChar(enChiffre);
-				tabDecod[i-1] = newLettre;
-				Console.WriteLine($"Nouvelle lettre :{newLettre}");
-			}
+				foreach (var item in tabDecod)
+				{
+				Console.Write($"{item}");
+				}
+				Console.WriteLine($" // clé: +{count++}");
 
-			
+				cle++;
+				
+				} while (cle <= 26);
 
-			foreach (var item in tabDecod)
-			{
-			Console.Write($"{item}");
-			}
-			Console.WriteLine(" ");
 
+
+
+			/// " Dans le fronton d'un temple antique,
+			/// Sur le fond bleu du ciel attique,
+			/// Par de lentes métamorphoses "
 			
 
 		}
