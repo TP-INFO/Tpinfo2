730881bdb2afba128c4691908fdb397c639903f7
LucNtonga <luc@LUCNTONGA87BB>
2017-03-29 22:59:36 +0200

    v2.0 testé sur logique de décodage (hors args)

--- Tpinfo2/Program.cs
+++ Tpinfo2/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Tpinfo2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string phraseCode = "Ssfksdwsxjgflgfsvsmfslwehdwssflaimw";
+			//string phraseCode = "zzzzzzzzzzzzzzzzzzzzzzzzzzz";
+			int i = 0;
+			char[] tabDecod = new char[phraseCode.Length];
+			 
+			int cle = 8;
+
+
+			foreach (char c in phraseCode)
+			{
+				int nChiffre = phraseCode[i];
+				Console.WriteLine(nChiffre);
+				int enChiffre = nChiffre + cle;
+
+				if (enChiffre > 122)
+				{
+					enChiffre = (enChiffre + (cle - (26+cle)));
+				}
+
+				i++;
+				Console.WriteLine($"lettre originale :{phraseCode[i - 1]}");
+				Console.WriteLine($" :{enChiffre}");
+				char newLettre = Convert.ToChar(enChiffre);
+				tabDecod[i-1] = newLettre;
+				Console.WriteLine($"Nouvelle lettre :{newLettre}");
+			}
+
+			
+
+			foreach (var item in tabDecod)
+			{
+			Console.Write($"{item}");
+			}
+			Console.WriteLine(" ");
+
+			
+
+		}
+	}
 }
