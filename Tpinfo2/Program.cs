13e7ccf0cd36420fecca8841c4e08f03e938ff6a
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-04-01 09:53:51 +0200

    InitialCommit

--- Tpinfo2/Program.cs
+++ Tpinfo2/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tpinfo2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
+namespace message {
+  class Program {
+    static void Main(string[] args) {
+
+      if(args.Length != 1) {
+        Utils.DisplayMessage(Utils.HelpMessage);
+        System.Environment.Exit(0);
+      }
+
+      // 
+      switch(args[0].ToUpper()) {
+        case "ENCODE":
+          Utils.EncodeDialog(); 
+          break;
+        case "DECODE":
+          Utils.DecodeDialog();
+          break;
+        default:
+          Utils.DisplayMessage("Unknowd command\n" + Utils.HelpMessage); 
+          break;
+      }
+
+
+
+
     }
+  }
 }
---
+++ Tpinfo2/Utils.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace message {
+  static class Utils {
+
+    static public string HelpMessage = "Utilisation: \n" +
+                                "message encode/decode\n";
+
+
+
+    /// <summary>
+    /// Starts a session of encoding. Prompts the user to enter a message 
+    /// and calls encoding subroutine 
+    /// </summary>
+    public static void EncodeDialog() {
+      int shiftAmount = 5;  //
+      string userInput = null;
+      bool validInput = false;
+      Console.WriteLine("Please enter a message to be encoded: ");
+      try {
+        while(!validInput) {
+          userInput = Console.ReadLine();
+
+          // check if string contains only letter
+          if(IsAlphaOnly(userInput)) {
+            string encodedMessage = EncodeDialog(userInput, shiftAmount);
+            Console.WriteLine("The encoded message: ");
+            Console.WriteLine(encodedMessage);
+            // TODO : Add possiblity for multiple message decoding session 
+
+          }
+          else {// not valid input 
+            Console.WriteLine("input not valid. Try again. Enter 0 to quit ");
+            userInput = Console.ReadLine();
+            if(userInput != "0")
+              validInput = true;  
+
+            
+
+          }
+
+
+
+
+
+        }
+
+
+
+
+      }
+      catch(Exception) {
+
+        throw;
+      }
+
+
+
+
+    }
+
+
+    /// <summary>
+    /// Encode a message using shifting technique
+    /// </summary>
+    /// <param name="userInput"></param>
+    /// <param name="shiftAmount"></param>
+    /// <returns></returns>
+    private static string EncodeDialog(string userInput, int shiftAmount) {
+      char[] charMessage = userInput.ToCharArray();
+      for(int i = 0; i < charMessage.Length; i++) {
+        Char.ToLower(charMessage[i]);  // TODO: add support to upper case input
+        charMessage[i] = (char)((charMessage[i] + shiftAmount) % 26 + 'a');
+      }
+      return new string(charMessage); 
+    }
+
+    internal static void DecodeDialog() {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Checks if the string contains letters only 
+    /// </summary>
+    /// <param name="str"><value>string to be tested</value></param>
+    /// <returns></returns>
+    private static bool IsAlphaOnly(string str) {
+      for(int i = 0; i < str.Length; i++) {
+        if(!Char.IsLetter(str[i]))
+          return false;
+      }
+      return true; 
+    }
+
+
+    /// <summary>
+    /// Displays a help message on the console 
+    /// </summary>
+    /// <param name="msg"><value> message to be displaye</value></param>
+    public static void DisplayMessage(string msg) {
+      Console.WriteLine(msg);
+    }
+
+
+
+
+
+
+
+  }// Utils
+}
